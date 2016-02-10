9f8099568595d7a53d8848e3d0e8b651922b97ef
BenjaminLegrand <benjamin.legrand93@gmail.com>
2016-02-10 17:08:38 +0200

    Added model for UserProfile

--- user/models.py
+++ user/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
+from level.models import Level
 
 # Create your models here.
+class UserProfile(models.Model):
+	user = models.OneToOneField(User, on_delete=models.CASCADE)
+	age = models.PositiveIntegerField()
+	city = models.CharField(max_length=50)
+	levels = models.ManyToManyField(Level, on_delete=models.CASCADE)
+	coachs = models.ManyToManyField('self', symmetrical=False)
+	trainee = models.ManyToManyField('self', symmetrical=False)
