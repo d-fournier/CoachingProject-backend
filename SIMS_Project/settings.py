2ea8f31eb9020c6ea3148e099e67354e33b4fea1
BenjaminLegrand <benjamin.legrand93@gmail.com>
2016-02-10 11:55:08 +0200

    Created apps

--- SIMS_Project/settings.py
+++ SIMS_Project/settings.py
@@ -38,7 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'coach_me',
+    'user',
+    'level',
+    'sport',
+    'message',
 ]
 
 MIDDLEWARE_CLASSES = [
