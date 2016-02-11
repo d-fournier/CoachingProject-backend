ba5fd3996db57c12a705ad07cd168d7952022326
BenjaminLegrand <benjamin.legrand93@gmail.com>
2016-02-11 19:03:20 +0200

    Removed /api/coachs URL.
    Filtering users by username and by coach/not coach added

--- user/views.py
+++ user/views.py
@@ -5,12 +5,14 @@ from rest_framework import viewsets, permissions
 
 # Create your views here.
 class UserProfileViewSet(viewsets.ModelViewSet):
-	queryset = UserProfile.objects.all()
 	serializer_class = UserProfileSerializer
 	permission_classes= [permissions.IsAuthenticatedOrReadOnly]
 
-
-class CoachsViewSet(viewsets.ModelViewSet):
-	queryset = UserProfile.objects.filter(isCoach=True)
-	serializer_class = UserProfileSerializer
-	permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+	def get_queryset(self):
+		queryset = UserProfile.objects.all()
+		username = self.request.query_params.get('username', None)
+		if username is not None :
+			queryset = queryset.filter(user__username=username)
+		coach = self.request.query_params.get('isCoach', False)
+		queryset = queryset.filter(isCoach=coach)
+		return queryset
