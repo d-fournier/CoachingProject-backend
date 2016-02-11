584aadcdf17ec2cad8c5425b0e2683ce27de6977
BenjaminLegrand <benjamin.legrand93@gmail.com>
2016-02-11 16:10:24 +0200

    Added model + view for Messages

--- message/models.py
+++ message/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from user.models import UserProfile
 
 # Create your models here.
+class Message(models.Model):
+	content = models.TextField()
+	from_user = models.ForeignKey(UserProfile, on_delete=models.SET_DEFAULT, default=-1)
--- message/views.py
+++ message/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .serializers import UserProfileSerializer
+from .models import UserProfile
+from rest_framework import viewsets, permissions
 
 # Create your views here.
+class MessageViewSet(viewsets.ModelViewSet):
+	queryset = Message.objects.all()
+	serializer_class = MessageSerializer
+	permission_classes= [permissions.IsAuthenticatedOrReadOnly]
