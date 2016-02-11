e4c71739ebd942fbc814f7f87f179f11c1b0e979
BenjaminLegrand <benjamin.legrand93@gmail.com>
2016-02-11 16:57:38 +0200

    Removed Conversation, finished message

--- message/models.py
+++ message/models.py
@@ -6,4 +6,4 @@ from conversation.models import Conversation
 class Message(models.Model):
 	content = models.TextField()
 	from_user = models.ForeignKey(UserProfile, on_delete=models.SET_DEFAULT, default=-1)
-	to_conversation = models.ForeignKey(Conversation, on_delete=models.CASCADE)
+	to_conversation = models.ForeignKey(Conversation, on_delete=models.CASCADE, blank=False)
