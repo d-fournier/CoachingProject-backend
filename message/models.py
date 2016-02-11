54ae7596c7bca5a1ac6ceb784d84bd19d96b926f
BenjaminLegrand <benjamin.legrand93@gmail.com>
2016-02-11 17:09:28 +0200

    Removed Conversation from Message model, will be added again later

--- message/models.py
+++ message/models.py
@@ -1,9 +1,7 @@
 from django.db import models
 from user.models import UserProfile
-from conversation.models import Conversation
 
 # Create your models here.
 class Message(models.Model):
 	content = models.TextField()
 	from_user = models.ForeignKey(UserProfile, on_delete=models.SET_DEFAULT, default=-1)
-	to_conversation = models.ForeignKey(Conversation, on_delete=models.CASCADE, blank=False)
