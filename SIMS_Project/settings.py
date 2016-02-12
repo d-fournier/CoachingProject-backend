4892837ab66237be6798f4b7caadbc11a8536640
d-fournier <donof43@gmail.com>
2016-02-12 06:44:24 -0500

    Fix Static folder

--- SIMS_Project/settings.py
+++ SIMS_Project/settings.py
@@ -134,7 +134,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
+STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 ### HEROKU config
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
