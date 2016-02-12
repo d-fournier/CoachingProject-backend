6c4e6fea661fa4a87d7e151d5501591e945155f8
SegoleneM <segolene.minjard@gmail.com>
2016-02-12 12:18:10 +0100

    Add description to UserProfile Delete coachs and trainees in UserProfile Delete CoachsViewset import in urls

--- user/models.py
+++ user/models.py
@@ -8,9 +8,8 @@ class UserProfile(models.Model):
 	age = models.PositiveIntegerField()
 	isCoach = models.BooleanField(default=False)
 	city = models.CharField(max_length=50)
+	description = models.CharField(max_length=400)
 	levels = models.ManyToManyField(Level, blank=True)
-	coachs = models.ManyToManyField('self', related_name='%(class)s_coachs', symmetrical=False, blank=True)
-	trainees = models.ManyToManyField('self', related_name='%(class)s_trainees', symmetrical=False, blank=True)
 
 	def __str__(self):
 		return self.user.username
