bbe6d72790c78e53644e97b1ccb4d9711b068487
SegoleneM <segolene.minjard@gmail.com>
2016-02-13 16:59:59 +0100

    Modify Relation model

--- relation/models.py
+++ relation/models.py
@@ -1,7 +1,12 @@
 from django.db import models
 from user.models import UserProfile
+from sport.models import Sport
 
 # Create your models here.
 class Relation(models.Model):
 	coach = models.ForeignKey(UserProfile,related_name='%(class)s_coach', blank=False)
 	trainee = models.ForeignKey(UserProfile,related_name='%(class)s_trainee', blank=False)
+	requestStatus = models. NullBooleanField()
+	sport = models.ForeignKey(Sport, blank=False)
+	comment = models.CharField(max_length=200, blank=True)
+	date =  models.DateField(auto_now=False, auto_now_add=True)
