8a8613db2af49e7f0f1c66c461c29f53b67ee3a5
SegoleneM <segolene.minjard@gmail.com>
2016-02-15 15:27:54 +0100

    POST message only for active relation including the user Modify settings for log in problem in Django interface

--- message/views.py
+++ message/views.py
@@ -3,6 +3,8 @@ from .serializers import MessageSerializer
 from .models import Message
 from rest_framework import viewsets, permissions
 from django.db.models import Q
+from rest_framework.response import Response
+from rest_framework import status
 
 # Create your views here.
 class MessageViewSet(viewsets.ModelViewSet):
@@ -12,4 +14,20 @@ class MessageViewSet(viewsets.ModelViewSet):
 	def get_queryset(self):
 		queryset = Message.objects.filter(Q(to_relation__coach__user=self.request.user)|Q(to_relation__trainee__user=self.request.user))
 		return queryset
+
+	def create(self,request):
+		serializer = self.get_serializer(data=request.data)
+
+		if serializer.is_valid():
+
+			relation = serializer.validated_data.get('to_relation')
+			if relation.requestStatus == True & ((relation.coach.user==self.request.user) | (relation.trainee.user==self.request.user)):
+				self.object = serializer.save()
+				self.object.save()
+				headers = self.get_success_headers(serializer.data)
+				return Response(serializer.data, status=status.HTTP_201_CREATED,
+                            headers=headers)
+			return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+		return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         
