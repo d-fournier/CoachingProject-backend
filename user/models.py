3f6d4e2c0becd5b2c3f5f8cb94048e05bd451d5b
BenjaminLegrand <benjamin.legrand93@gmail.com>
2016-02-15 10:32:34 +0200

    Forgot to stage

--- user/models.py
+++ user/models.py
@@ -5,10 +5,11 @@ from level.models import Level
 # Create your models here.
 class UserProfile(models.Model):
 	user = models.OneToOneField(User, on_delete=models.CASCADE)
+	displayName = models.CharField(max_length=50)
 	age = models.PositiveIntegerField()
 	isCoach = models.BooleanField(default=False)
 	city = models.CharField(max_length=50)
-	description = models.CharField(max_length=400, blank=True)
+	description = models.TextField(max_length=400, blank=True)
 	levels = models.ManyToManyField(Level, blank=True)
 
 	def __str__(self):
