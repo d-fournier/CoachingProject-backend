bc4fabbb309ae9306f5d3db8fd2674f5f883c2ff
SegoleneM <segolene.minjard@gmail.com>
2016-02-16 10:57:52 +0100

    Get messages of a relation Add time in messages

--- message/models.py
+++ message/models.py
@@ -7,3 +7,4 @@ class Message(models.Model):
 	content = models.TextField()
 	from_user = models.ForeignKey(UserProfile, on_delete=models.SET_DEFAULT, default=-1)
 	to_relation = models.ForeignKey(Relation, on_delete=models.CASCADE, null=True, blank=True)
+	time = models.DateTimeField(auto_now_add=True)
