8c3c157d68aac6fa81683060e235d09780950944
BenjaminLegrand <benjamin.legrand93@gmail.com>
2016-02-16 11:03:08 +0200

    Modified all serializers and add __str__ to relation

--- relation/models.py
+++ relation/models.py
@@ -6,7 +6,10 @@ from sport.models import Sport
 class Relation(models.Model):
 	coach = models.ForeignKey(UserProfile,related_name='%(class)s_coach', blank=False)
 	trainee = models.ForeignKey(UserProfile,related_name='%(class)s_trainee', blank=False)
-	requestStatus = models. NullBooleanField()
+	requestStatus = models.NullBooleanField()
 	sport = models.ForeignKey(Sport, blank=False)
 	comment = models.CharField(max_length=200, blank=True)
 	date =  models.DateField(auto_now=False, auto_now_add=True)
+
+	def __str__(self):
+		return coach.user.username+' <-> '+trainee.user.username
