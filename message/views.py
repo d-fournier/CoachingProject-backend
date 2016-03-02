6a104e022c23fb300af1151bfb98c333a114456c
SegoleneM <segolene.minjard@gmail.com>
2016-03-02 10:10:59 +0100

    Patch supported

--- message/views.py
+++ message/views.py
@@ -40,7 +40,7 @@ class MessageViewSet(viewsets.ModelViewSet):
 					self.object.save()
 					headers = self.get_success_headers(serializer.data)
 					return Response(serializer.data, status=status.HTTP_201_CREATED,
-	                            headers=headers)
+								headers=headers)
 				return Response('Either the relation is not confirmed or you are not involved in it', status=status.HTTP_400_BAD_REQUEST)
 			
 			if group is not None :
@@ -50,9 +50,40 @@ class MessageViewSet(viewsets.ModelViewSet):
 					self.object.save()
 					headers = self.get_success_headers(serializer.data)
 					return Response(serializer.data, status=status.HTTP_201_CREATED,
-	                            headers=headers)
+								headers=headers)
 				return Response('You are not a member of this group', status=status.HTTP_400_BAD_REQUEST)
 
 
 		return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-        
+		
+	def update(self, request, *args, **kwargs):
+		instance = self.get_object()
+		if instance.to_relation is not None and instance.to_group is not None :
+			return Response('You cannot have a relation and a group for the same message, one should be null', status=status.HTTP_400_BAD_REQUEST)
+		if instance.to_relation is not None :
+			if instance.to_relation.requestStatus == True & ((instance.to_relation.coach.user==request.user) | (instance.to_relation.trainee.user==request.user)):
+				if(request.data.get("is_pinned")!= None):
+					instance.is_pinned=request.data.get("is_pinned")
+					instance.save()			
+					serializer = self.get_serializer(instance, data={'is_pinned':request.data.get('is_pinned')}, partial=True)
+					serializer.is_valid(raise_exception=True)
+					self.perform_update(serializer)
+					headers = self.get_success_headers(serializer.data)
+					return Response(serializer.data, status=status.HTTP_200_OK,headers=headers)
+				return Response('Only the is_pinned value can be modified', status=status.HTTP_400_BAD_REQUEST)	
+			return Response('Either the relation is not confirmed or you are not involved in it', status=status.HTTP_400_BAD_REQUEST)
+			
+		if instance.to_group is not None :
+			up = UserProfile.objects.get(user=request.user)
+			if up in instance.to_group.members.all():	
+				if(request.data.has_key("is_pinned")!=None):
+					instance.is_pinned=request.data.get("is_pinned")
+					instance.save()			
+					serializer = self.get_serializer(instance, data={'is_pinned':request.data.get('is_pinned')}, partial=True)
+					serializer.is_valid(raise_exception=True)
+					self.perform_update(serializer)
+					headers = self.get_success_headers(serializer.data)
+					return Response(serializer.data, status=status.HTTP_200_OK,headers=headers)
+				return Response('Only the is_pinned value can be modified', status=status.HTTP_400_BAD_REQUEST)	
+			return Response('You are not a member of this group', status=status.HTTP_400_BAD_REQUEST)
+		return Response('Message need to have either relation or group', status=status.HTTP_400_BAD_REQUEST)
