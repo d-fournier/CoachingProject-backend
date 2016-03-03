1c2054274105499d0247b718ef70ae2301047675
d-fournier <d-fournier@users.noreply.github.com>
2016-03-03 19:41:03 +0100

    Update wsgi.py

--- SIMS_Project/wsgi.py
+++ SIMS_Project/wsgi.py
@@ -15,4 +15,5 @@ from dj_static import Cling
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "SIMS_Project.settings")
 
 # application = get_wsgi_application()
-application = Cling(get_wsgi_application())
+# application = Cling(get_wsgi_application())
+application = Cling(MediaCling(get_wsgi_application()))
