cdd056d64bf7a64c1681d3c1d37e015ebc663cf4
d-fournier <d-fournier@users.noreply.github.com>
2016-03-04 00:10:11 +0100

    Update settings.py

--- SIMS_Project/settings.py
+++ SIMS_Project/settings.py
@@ -160,9 +160,10 @@ if awsS3AccessKeyId and awsS3SecretAccessKey:
     S3_USE_SIGV4=True
     AWS_S3_SECURE_URLS = False       # use http instead of https
     AWS_QUERYSTRING_AUTH = False     # don't add complex authentication-related query parameters for requests
-    AWS_STORAGE_BUCKET_NAME = 'coachingapplication'
+    AWS_STORAGE_BUCKET_NAME = 'coachingapplication.media'
     AWS_S3_ACCESS_KEY_ID = awsS3AccessKeyId    # enter your access key id
     AWS_S3_SECRET_ACCESS_KEY = awsS3SecretAccessKey # enter your secret access key
+    MEDIA_URL = 'http://%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
 else:
     MEDIA_ROOT = 'media'
     MEDIA_URL ='/media/'
