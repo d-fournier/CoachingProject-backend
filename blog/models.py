60ceb450f83b345f1d7f71f33dcf607fde81c4dd
d-fournier <donof43@gmail.com>
2016-03-10 12:19:48 -0500

    Add Blog Application Add Blog post Model

---
+++ blog/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from user.models import UserProfile
+from sport.models import Sport
+
+def blog_directory_path(instance, filename):
+	return 'blog/post/{0}_{1}'.format(instance.id,filename)
+
+# Create your models here.
+class Post(models.Model):
+	author = models.ForeignKey(UserProfile, on_delete=models.SET_NULL, null=True)
+	date = models.DateTimeField(auto_now_add=True)
+	sport = models.ForeignKey(Sport, blank=False)
+    title = models.CharField(max_length=140)
+	description = models.TextField(max_length=400, blank=True)
+	content = models.TextField()
+	picture = models.ImageField(upload_to=blog_directory_path, default=None, null=True, blank=True)
