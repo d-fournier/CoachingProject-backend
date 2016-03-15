8b4a88f2a677e1a4c161cad7a288915056d946ae
BenjaminLegrand <benjamin.legrand93@gmail.com>
2016-03-15 11:57:33 +0100

    appends > append

--- group/views.py
+++ group/views.py
@@ -185,7 +185,7 @@ class GroupViewSet(viewsets.ModelViewSet):
 				admins = GroupStatus.objects.filter(group=group,status=GroupStatus.ADMIN)
 				users = []
 				for a in admins:
-					users.appends(a.user)
+					users.append(a.user)
 				scripts.sendGCMGroupJoin(users,group)
 				return Response('Demand created and sent to the group', status=status.HTTP_201_CREATED)
 		return Response('You are not connected', status=status.HTTP_401_UNAUTHORIZED)
